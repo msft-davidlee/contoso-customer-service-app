429907dfadbfb84395409aafde49e2bb78204635
David Lee <leedavid@microsoft.com>
2021-11-09 08:28:09 -0600

    add a ping controller for health check

---
+++ DemoPartnerAPI/Controllers/PingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DemoPartnerAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public object Get()
+        {
+            var model = new
+            {
+                Value = "pong",
+                Timestamp = DateTime.UtcNow,
+                Server = Environment.MachineName,
+                OS = Environment.OSVersion
+            };
+
+            return model;
+        }
+    }
+}
