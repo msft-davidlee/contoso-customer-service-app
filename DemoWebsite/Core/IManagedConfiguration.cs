d7d2e1c1731c0397a41176669f1a53d8d2ac4be5
ENG KEONG DAVID LEE <36802330+seekdavidlee@users.noreply.github.com>
2021-12-28 16:38:59 -0600

    update to use .NET 6 (#6)

---
+++ DemoWebsite/Core/IManagedConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace DemoWebsite.Core
+{
+    public interface IManagedConfiguration
+    {
+        string Version { get; }
+        string MachineName { get; }
+    }
+
+    public class ManagedConfiguration : IManagedConfiguration
+    {
+        private readonly string _version;
+        private readonly string _machineName;
+
+        public ManagedConfiguration(string version, string machineName)
+        {
+            _version = version;
+            _machineName = machineName;
+        }
+
+        public string Version
+        {
+            get
+            {
+                return string.IsNullOrEmpty(_version) ? "1.0" : _version;
+            }
+        }
+
+        public string MachineName
+        {
+            get
+            {
+                return string.IsNullOrEmpty(_machineName) ? "unknown" : _machineName;
+            }
+        }
+    }
+}
