c055aa450768366f43d61a5a591e3eaf3ce8bac8
David Lee <leedavid@microsoft.com>
2022-04-25 11:10:37 -0500

    add quotes

--- Deployment/Build.ps1
+++ Deployment/Build.ps1
@@ -108,7 +108,7 @@ for ($i = 0; $i -lt $apps.Length; $i++) {
 
     if ($shouldBuild -eq $true) {
         # Build your app with ACR build command
-        az acr build --image $imageName --build-arg version="$APP_VERSION" -r $AcrName --file ./$path/Dockerfile .
+        az acr build --image $imageName --build-arg version="'$APP_VERSION'" -r $AcrName --file ./$path/Dockerfile .
     
         if ($LastExitCode -ne 0) {
             throw "An error has occured. Unable to build image."
